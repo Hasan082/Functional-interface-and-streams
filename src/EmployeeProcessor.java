fcfd76c03cc1f2e470459a0ec136e4eb03675279
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-01 22:32:59 +0600

    [ADDED] Funcational interface and sreatm method added

--- src/EmployeeProcessor.java
+++ src/EmployeeProcessor.java
@@ -1,16 +1,25 @@
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class EmployeeProcessor {
     public static void main(String[] args) throws Exception {
-        List<Employee> emploList = Arrays.asList(
+        List<Employee> employeList = Arrays.asList(
                 new Employee("John", 28, "HR", 52000),
                 new Employee("Emily", 34, "Engineering", 78000),
                 new Employee("Michael", 45, "Engineering", 105000),
                 new Employee("Sarah", 36, "HR", 62000),
                 new Employee("David", 25, "Marketing", 48000));
 
-        Function<Employee, String> nameAndDepartment = emp -> emp.getName() + "-" + emp.getDepartment();
+        // Implemnet Functional interfaces here
+        Function<Employee, String> nameAndDepartment = emp -> emp.getName() + " - " + emp.getDepartment();
+
+        // Create a list of emplaye and departmet concated
+        List<String> nameAndDepList = employeList.stream().map(nameAndDepartment).collect(Collectors.toList());
+        System.out.println("Name - Department List: " + nameAndDepList);
+
     }
 }
